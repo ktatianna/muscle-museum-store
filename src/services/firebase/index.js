19d1e0d65f474075b5658b1e794ec571161407c2
Tati <ktgomezgaitan@gmail.com>
2025-03-21 15:44:14 -0500

    migrated to vite

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_authDomain,
+  projectId: import.meta.env.REACT_APP_projectId,
+  storageBucket: import.meta.env.REACT_APP_storageBucket,
+  messagingSenderId: import.meta.env.REACT_APP_messagingSenderId,
+  appId: import.meta.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const firestoreDb = getFirestore(app) 
+export const firestoreDb = getFirestore(app)
